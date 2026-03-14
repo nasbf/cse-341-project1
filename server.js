872ee470a2502459e0fa655aeb4eb3f2e2a2aa41
nasbf <mo0ma@hotmail.com>
2026-03-14 17:58:27 -0500

    cors

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const app = express();
 const port = process.env.PORT ||  8080;
 
 
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  allowedHeaders: ["Content-Type","Authorization"]
 
-app.use(cors());
+})));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
