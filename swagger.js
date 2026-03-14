4bad5b7e84462a799114cf594d7d227d8b5b48e0
nasbf <mo0ma@hotmail.com>
2026-03-14 13:09:07 -0500

    update

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API documentation for contacts'
   },
   host: 'localhost:8080',
-  schemes: ['http']
+  schemes: ['https', 'http']
 };
 
 const outputFile = './swagger.json';
